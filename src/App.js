5bece39489501c32bee689e09825280da3e2e3be
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2023-11-08 08:45:51 -0300

    ¨¨¨¨

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './assets/pages/Home';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../components/Header/index'
+import Hero from '../components/Hero/index'
+import Counts from '../components/Counts/index'
+import Numbers from '../components/Numbers/index'
+import Freatures from '../components/Freatures/index'
+
+function Home() {
+  return (
+    <>
+        <Header/>
+        <Hero/>
+        <Counts/>
+        <Numbers/>
+        <Freatures/>
+        
+    </>
+    
+  )
+}
+
+export default Home
